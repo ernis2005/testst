35fed6d3870d76d574fe517021040b0263afcc9e
Janarbek <janarbek.bekmuyrzauulu.alt@gmail.com>
2023-10-31 15:16:30 +0600

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-
+import s from './page.module.scss'
 export default function Home() {
   return (
     <div>
---
+++ components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import s from './page.module.scss'
+export default function Header() {
+  return (
+    <div className={` Contend ${s.Header}`}>Header</div>
+  )
+}
